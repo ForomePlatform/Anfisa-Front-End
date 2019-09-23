2dbe99ce1fa1acffb5411a1d63c1b77e144fd293
11235andrew <sunts_andrew@mail.ru>
2019-09-23 19:13:52 +0300

    From /statunit to /statunits 2.

--- src/store/actions.js
+++ src/store/actions.js
@@ -481,7 +481,7 @@ export function getStatList(context, { conditions = null, filter = null }) {
 export function getZygosityByFamily(context, { name, family }) {
     const params = new URLSearchParams();
     params.append('ws', context.state.workspace);
-    params.append('units', [name]);
+    params.append('units', JSON.stringify([name]));
     params.append('ctx', JSON.stringify({ problem_group: family }));
     if (context.state.currentConditions.length) {
         params.append('conditions', JSON.stringify(context.state.currentConditions));
