93132424749687c6f4c2241ddef50a5a3de05a6a
11235andrew <sunts_andrew@mail.ru>
2019-10-01 23:47:07 +0300

    [AN-136] Base editor logarithmic.

--- src/components/filterModal/statsList/BaseEditorLogarithmic.vue
+++ src/components/filterModal/statsList/BaseEditorLogarithmic.vue
@@ -3,7 +3,7 @@
         <div class="float-editor_inputs">
             <b-form-input
               type="number"
-              v-model.number="selectedMin"
+              v-model.number="Math.round(selectedMin * 10000) / 10000"
               :step="0.0001"
               :min="min"
               :max="selectedMax"
@@ -12,7 +12,7 @@
             &mdash;
             <b-form-input
               type="number"
-              v-model.number="selectedMax"
+              v-model.number="Math.round(selectedMax * 10000) / 10000"
               :step="0.0001"
               :min="selectedMin"
               :max="max"
@@ -24,11 +24,13 @@
           :enable-cross="false"
           :marks="true"
           :data="marks"
-          :min="min"
-          :max="max"
+          :min="closestLeftMark(preselectedMin)"
+          :max="closestRightMark(preselectedMax)"
+          :key="key"
           :interval="0.0001"
           @change="changeValues"
           tooltip="none"
+          @drag-end="onDragEnd"
           :class="[defaultSlider ? 'float-editor_slider__default' : '', 'float-editor_slider']"
         />
         <div class="float-editor_button" @click="addData">
@@ -51,8 +53,11 @@ export default {
         return {
             min: MIN,
             max: MAX,
-            selectedMin: this.preselectedMin || MIN,
-            selectedMax: this.preselectedMax || MAX,
+            currentMin: this.preselectedMin,
+            currentMax: this.preselectedMax,
+            selectedMin: this.closestLeftMark(this.preselectedMin),
+            selectedMax: this.closestRightMark(this.preselectedMax),
+            key: 0,
         };
     },
     methods: {
@@ -60,8 +65,27 @@ export default {
             this.onSubmit(this.selectedMin, this.selectedMax);
         },
         changeValues([min, max]) {
-            this.selectedMin = min;
-            this.selectedMax = max;
+            this.currentMin = min;
+            this.currentMax = max;
+            this.selectedMin = (min < this.preselectedMin ? this.closestLeftMark(this.preselectedMin) : this.closestLeftMark(min));
+            this.selectedMax = (max > this.preselectedMax ? this.closestRightMark(this.preselectedMax) : this.closestRightMark(max));
+        },
+                closestLeftMark(value) {
+            const arr = LOG_EDITOR_MARKS;
+            for (let i=0; i<arr.length; i++) {
+                if (value < arr[i]) return arr[i<1 ? 0 : i-1];
+            }
+            return arr[0];
+        },
+        closestRightMark(value) {
+            const arr = LOG_EDITOR_MARKS;
+            for (let i=0; i<arr.length; i++) {
+                if (value <= arr[i]) return arr[i];
+            }
+            return arr[0];
+        },
+        onDragEnd() {
+            this.key += 1;
         },
     },
     computed: {
@@ -69,11 +93,7 @@ export default {
             return LOG_EDITOR_MARKS;
         },
         sliderValues() {
-            if (LOG_EDITOR_MARKS.includes(this.selectedMin)
-              && LOG_EDITOR_MARKS.includes(this.selectedMax)) {
-                return [this.selectedMin, this.selectedMax];
-            }
-            return [this.min, this.max];
+            return [this.selectedMin, this.selectedMax];
         },
         defaultSlider() {
             return this.min === this.sliderValues[0] && this.max === this.sliderValues[1];
@@ -87,12 +107,12 @@ export default {
     watch: {
         preselectedMin(newVal, oldVal) {
             if (newVal !== oldVal) {
-                this.selectedMin = newVal;
+                this.selectedMin = this.closestLeftMark(newVal);
             }
         },
         preselectedMax(newVal, oldVal) {
             if (newVal !== oldVal) {
-                this.selectedMax = newVal;
+                this.selectedMax = this.closestRightMark(newVal);
             }
         },
     },
