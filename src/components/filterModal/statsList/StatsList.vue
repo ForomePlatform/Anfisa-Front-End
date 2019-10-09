7af69d752f94716eb9bc091e5e6692a2703029cc
11235andrew <sunts_andrew@mail.ru>
2019-10-09 20:56:01 +0300

    [AN-139] Some changes for Eslint.

--- src/components/filterModal/statsList/StatsList.vue
+++ src/components/filterModal/statsList/StatsList.vue
@@ -181,7 +181,8 @@ export default {
                 && stat.data.filter(subStat => this.oCurrentConditions[subStat.name]).length));
         },
         showStat(stat) {
-            return !this.hasProblemGroup(stat) && (!this.nonzeroChecked || checkNonzeroStat(stat, this.searchQuery));
+            return !this.hasProblemGroup(stat) &&
+                (!this.nonzeroChecked || checkNonzeroStat(stat, this.searchQuery));
         },
         primaryDisabled(stat) {
             return !this.filledStat(stat) && (
@@ -191,8 +192,8 @@ export default {
         },
         inheritanceHandler(stat) {
             if (stat.title === 'Inheritance') {
-                return !stat.data.filter(item => !this.hasProblemGroup(item) && this.filteredData(item).variants).length
-                    || !stat.data.length;
+                return !stat.data.filter(item => !this.hasProblemGroup(item) &&
+                    this.filteredData(item).variants).length || !stat.data.length;
             }
             return !stat.data.length;
         },
@@ -200,7 +201,8 @@ export default {
             return (stat.type === STAT_TYPE_ZYGOSITY) && (stat.data.variants === null);
         },
         secondaryDisabled(stat) {
-            return this.hasProblemGroup(stat) || !this.filledStat(stat) && !this.showStat(stat) && this.inheritanceHandler(stat);
+            return this.hasProblemGroup(stat) || !this.filledStat(stat) && !this.showStat(stat) &&
+                this.inheritanceHandler(stat);
         },
         expandPreselectedStats() {
             const isStringTrue = value => value === 'true';
