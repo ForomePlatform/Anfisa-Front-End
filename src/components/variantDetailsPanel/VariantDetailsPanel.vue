51e5a314ee462b750a682ae0d2feee014688a9df
11235andrew <sunts_andrew@mail.ru>
2019-12-04 00:47:08 +0300

    [AN-216] Cohorts.

--- src/components/variantDetailsPanel/VariantDetailsPanel.vue
+++ src/components/variantDetailsPanel/VariantDetailsPanel.vue
@@ -95,6 +95,9 @@ export default {
                 {
                     x: 0, y: 30, w: 6, h: 5, i: 9, name: 'VCF', secondary: true,
                 },
+                {
+                    x: 0, y: 35, w: 6, h: 5, i: 10, name: 'Cohorts', secondary: true,
+                },
             ],
         };
     },
