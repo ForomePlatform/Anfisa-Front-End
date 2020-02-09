09a5842476c3ccba11e57076d763513eb32322fa
11235andrew <sunts_andrew@mail.ru>
2020-02-09 17:12:32 +0300

    Reject the previous commit.

--- src/components/filterModal/statsList/StatsList.vue
+++ src/components/filterModal/statsList/StatsList.vue
@@ -186,8 +186,6 @@ export default {
                 && stat.data.filter(subStat => this.oCurrentConditions[subStat.name]).length));
         },
         showStat(stat) {
-            console.log(`Show.${stat.name}: ${!this.hasProblemGroup(stat) &&
-                (!this.nonzeroChecked || checkNonzeroStat(stat, this.searchQuery))}`);
             return !this.hasProblemGroup(stat) &&
                 (!this.nonzeroChecked || checkNonzeroStat(stat, this.searchQuery));
         },
